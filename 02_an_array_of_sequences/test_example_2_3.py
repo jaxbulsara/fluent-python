36b251e90765d04108f719513c6442b3eafb2ae5
Jay Bulsara <jaxbulsara@gmail.com>
2021-09-08 21:10:44 +0000

    Added notes about readability of each strategy.

--- 02_an_array_of_sequences/test_example_2_3.py
+++ 02_an_array_of_sequences/test_example_2_3.py
@@ -7,12 +7,14 @@ result = [162, 163, 165, 8364, 164]
 
 
 def test_beyond_ascii_listcomp():
+    # Very readable
     beyond_ascii = [ord(s) for s in symbols if ord(s) > 127]
 
     assert beyond_ascii == result
 
 
 def test_beyond_ascii_filter_map():
+    # Not very readable
     beyond_ascii = list(filter(lambda c: c > 127, map(ord, symbols)))
 
     assert beyond_ascii == result
